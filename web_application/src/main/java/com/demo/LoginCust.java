98b0bb4b6313426af39af02f2f41d635dd3552d7
Prathmeshshingare <prathmeshshingare4@gmail.com>
2025-12-27 17:44:30 +0530

    servlet file

---
+++ web_application/src/main/java/com/demo/LoginCust.java
@@ -0,0 +1,64 @@
+package com.demo;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import com.mysql.cj.jdbc.MysqlDataSource;
+
+/**
+ * Servlet implementation class LoginCust
+ */
+@WebServlet("/log")
+public class LoginCust extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginCust() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		String user=request.getParameter("user");
+		String pwd=request.getParameter("pwd");
+		
+		Connection conn;
+		PreparedStatement prp;
+		MysqlDataSource ds=new MysqlDataSource();
+		ds.setUser("root");
+		ds.setPassword("1234");
+		ds.setServerName("localhost");
+		ds.setDatabaseName("db_j2ee");
+		try {
+			 String qry="select * from customer where id=? and name=?";
+			conn=ds.getConnection();
+			prp=conn.prepareStatement(qry);
+			
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
